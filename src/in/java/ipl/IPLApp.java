8a569cc3884dd2391ffb4c0a09129241429d6650
bhramaramba_ds <bhramaramba_ds@THBSLAPTOP-728.thbs.india.com>
2021-09-02 11:14:20 +0530

    Project created

--- src/in/java/ipl/IPLApp.java
+++ src/in/java/ipl/IPLApp.java
@@ -3,7 +3,6 @@ package in.java.ipl;
 public class IPLApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("IPL 2021");
 		
 		System.out.println(" >>IPL TEAM - ROYAL CHALLENGERS BANGALORE. \n >>CAPTAIN - Virat Koli. \n >>WICKET KEEPER - AB de villers. \n >>BATSMAN - DevDUtt Padikal \n >>BOWLER - Yuzvendra Chahal \n >>Total of 266 sixes \n >> Net run rate is 0.171 \n >> not been knock out yet");
